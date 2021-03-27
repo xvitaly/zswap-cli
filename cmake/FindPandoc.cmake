7fa19989872dc4b3bef7cfe9504285480167e67f
Vitaly Zaitsev <vitaly@easycoding.org>
2021-03-27 12:45:09 +0100

    Introduced a new setting BUILD_MANPAGE.
    Moved all manpage generating logic to the assets.

--- cmake/FindPandoc.cmake
+++ cmake/FindPandoc.cmake
@@ -1,13 +1,9 @@
-find_program(
-    Pandoc_EXECUTABLE
-    NAMES pandoc
-    DOC "Pandoc main executable"
-)
-
-find_package_handle_standard_args(
-    Pandoc
-    FOUND_VAR Pandoc_FOUND
-    REQUIRED_VARS Pandoc_EXECUTABLE
-)
-
-mark_as_advanced(Pandoc_EXECUTABLE)
+if (BUILD_MANPAGE)
+    find_program(
+        Pandoc_EXECUTABLE
+        NAMES pandoc
+        DOC "Pandoc main executable"
+        REQUIRED
+    )
+    mark_as_advanced(Pandoc_EXECUTABLE)
+endif()
