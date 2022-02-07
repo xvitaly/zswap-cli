5eb954701e6b89cd62bdaaef3d7c66cb5b50be42
Vitaly Zaitsev <vitaly@easycoding.org>
2022-02-07 13:55:57 +0100

    Switched to ksystype.hpp instead of direct using private types.

--- src/lib/ksysinfo/ksysinfo.hpp
+++ src/lib/ksysinfo/ksysinfo.hpp
@@ -13,6 +13,8 @@
 
 #include <sys/sysinfo.h>
 
+#include "ksysinfo/ksystype.hpp"
+
 /**
  *  Class with properties and methods for working with memory
  * information using Linux kernel API.
@@ -29,67 +31,67 @@ public:
      *  Gets current uptime (in seconds since boot).
      * @returns Current uptime.
     */
-    __kernel_long_t& GetUptime();
+    ksystype::long_t& GetUptime();
 
     /**
      *  Gets total usable main memory size in bytes.
      * @returns Total usable main memory size.
     */
-    __kernel_ulong_t& GetTotalRam();
+    ksystype::ulong_t& GetTotalRam();
 
     /**
      *  Gets available memory size in bytes.
      * @returns Available memory size.
     */
-    __kernel_ulong_t& GetFreeRam();
+    ksystype::ulong_t& GetFreeRam();
 
     /**
      *  Gets amount of shared memory in bytes.
      * @returns Amount of shared memory.
     */
-    __kernel_ulong_t& GetSharedRam();
+    ksystype::ulong_t& GetSharedRam();
 
     /**
      *  Gets amount of memory used by buffers in bytes.
      * @returns Amount of memory used by buffers.
     */
-    __kernel_ulong_t& GetBufferedRam();
+    ksystype::ulong_t& GetBufferedRam();
 
     /**
      *  Gets total swap space size in bytes.
      * @returns Total swap space size.
     */
-    __kernel_ulong_t& GetTotalSwap();
+    ksystype::ulong_t& GetTotalSwap();
 
     /**
      *  Gets available swap space in bytes.
      * @returns Available swap space.
     */
-    __kernel_ulong_t& GetFreeSwap();
+    ksystype::ulong_t& GetFreeSwap();
 
     /**
      *  Gets the number of current processes.
      * @returns The number of current processes.
     */
-    __u16& GetProcessesCount();
+    ksystype::ushort_t& GetProcessesCount();
 
     /**
      *  Gets total high memory size in bytes.
      * @returns Total high memory size.
     */
-    __kernel_ulong_t& GetTotalHighMem();
+    ksystype::ulong_t& GetTotalHighMem();
 
     /**
      *  Gets available high memory size in bytes.
      * @returns Available high memory size.
     */
-    __kernel_ulong_t& GetFreeHighMem();
+    ksystype::ulong_t& GetFreeHighMem();
 
     /**
      *  Gets memory unit size in bytes.
      * @returns Memory unit size.
     */
-    __u32& GetMemUnitSize();
+    ksystype::uint_t& GetMemUnitSize();
 private:
     /**
      *  Stores instance of sysinfo structure.
