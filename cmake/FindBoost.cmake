39735e2a760d1f9cfca210d3fa61221dccbb1eb5
Vitaly Zaitsev <vitaly@easycoding.org>
2023-03-14 10:58:52 +0100

    Removed separate CMakeLists.txt files.

--- cmake/FindBoost.cmake
+++
@@ -1,8 +0,0 @@
-#
-# SPDX-FileCopyrightText: 2020-2022 EasyCoding Team and contributors
-#
-# SPDX-License-Identifier: MIT
-#
-
-set(Boost_USE_STATIC_LIBS ${LINK_BOOST_STATICALLY})
-find_package(Boost 1.65.0 COMPONENTS program_options REQUIRED)
