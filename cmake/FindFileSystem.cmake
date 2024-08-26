f04005a8dc07271a568851a07cc48df04e70449c
Vitaly <vitaly@easycoding.org>
2024-08-26 10:10:01 +0200

    Restored filesystem header check.

---
+++ cmake/FindFileSystem.cmake
@@ -0,0 +1,16 @@
+#
+# SPDX-FileCopyrightText: 2020-2024 EasyCoding Team and contributors
+#
+# SPDX-License-Identifier: MIT
+#
+
+include(CheckIncludeFileCXX)
+
+CHECK_INCLUDE_FILE_CXX(
+    "filesystem"
+    FILESYSTEM_HEADER_FOUND
+)
+
+if(NOT FILESYSTEM_HEADER_FOUND)
+    message(FATAL_ERROR "Current C++ compiler has no <filesystem> support!")
+endif()
