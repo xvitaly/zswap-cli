0ba14b37f40a1b9eb0283dfcb29e801e9287240f
Vitaly <vitaly@easycoding.org>
2024-10-12 15:13:55 +0200

    Added a new function for building manpages.

--- cmake/FindPandoc.cmake
+++ cmake/FindPandoc.cmake
@@ -12,4 +12,15 @@ if (BUILD_MANPAGE)
         REQUIRED
     )
     mark_as_advanced(Pandoc_EXECUTABLE)
+
+    function(build_manpage SrcName DestName)
+        add_custom_command(
+            OUTPUT "${DestName}"
+            COMMAND "${Pandoc_EXECUTABLE}"
+            ARGS "${SrcName}" -s -t man -o "${DestName}"
+            DEPENDS "${SrcName}"
+            COMMENT "Building the ${DestName} manpage using Pandoc."
+            VERBATIM
+        )
+    endfunction()
 endif()
