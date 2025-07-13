01d9eb41d19499604652e3a5ca83756a68947130
Vitaly <vitaly@easycoding.org>
2025-07-13 07:51:04 +0200

    Switched the ZSwapDebug class to use std::optional.

--- src/lib/zswapdebug/zswapdebug.cpp
+++ src/lib/zswapdebug/zswapdebug.cpp
@@ -11,56 +11,57 @@
 
 #include <filesystem>
 #include <fstream>
+#include <optional>
 #include <string>
 
 #include "zswapdebug/zswapdebug.hpp"
 
-unsigned long ZSwapDebug::GetPoolLimitHit() const
+std::optional<unsigned long> ZSwapDebug::GetPoolLimitHit() const
 {
     return ReadModuleDebugValue("pool_limit_hit");
 }
 
-unsigned long ZSwapDebug::GetPoolTotalSize() const
+std::optional<unsigned long> ZSwapDebug::GetPoolTotalSize() const
 {
     return ReadModuleDebugValue("pool_total_size");
 }
 
-unsigned long ZSwapDebug::GetRejectAllocFail() const
+std::optional<unsigned long> ZSwapDebug::GetRejectAllocFail() const
 {
     return ReadModuleDebugValue("reject_alloc_fail");
 }
 
-unsigned long ZSwapDebug::GetRejectCompressPoor() const
+std::optional<unsigned long> ZSwapDebug::GetRejectCompressPoor() const
 {
     return ReadModuleDebugValue("reject_compress_poor");
 }
 
-unsigned long ZSwapDebug::GetRejectKmemCacheFail() const
+std::optional<unsigned long> ZSwapDebug::GetRejectKmemCacheFail() const
 {
     return ReadModuleDebugValue("reject_kmemcache_fail");
 }
 
-unsigned long ZSwapDebug::GetRejectReclaimFail() const
+std::optional<unsigned long> ZSwapDebug::GetRejectReclaimFail() const
 {
     return ReadModuleDebugValue("reject_reclaim_fail");
 }
 
-unsigned long ZSwapDebug::GetRejectCompressFail() const
+std::optional<unsigned long> ZSwapDebug::GetRejectCompressFail() const
 {
     return ReadModuleDebugValue("reject_compress_fail");
 }
 
-unsigned long ZSwapDebug::GetSameFilledPages() const
+std::optional<unsigned long> ZSwapDebug::GetSameFilledPages() const
 {
     return ReadModuleDebugValue("same_filled_pages");
 }
 
-unsigned long ZSwapDebug::GetStoredPages() const
+std::optional<unsigned long> ZSwapDebug::GetStoredPages() const
 {
     return ReadModuleDebugValue("stored_pages");
 }
 
-unsigned long ZSwapDebug::GetWrittenBackPages() const
+std::optional<unsigned long> ZSwapDebug::GetWrittenBackPages() const
 {
     return ReadModuleDebugValue("written_back_pages");
 }
@@ -70,16 +71,12 @@ bool ZSwapDebug::IsDebugAvailable() const
     return std::filesystem::exists(ModuleDebugPath);
 }
 
-unsigned long ZSwapDebug::ReadModuleDebugValue(const std::string& Name) const
+std::optional<unsigned long> ZSwapDebug::ReadModuleDebugValue(const std::string& Name) const
 {
     const std::string FullPath = ModuleDebugPath + Name;
-    unsigned long Result = 0UL;
-
-    if (std::filesystem::exists(FullPath))
-    {
-        std::ifstream ZSwapSysFs(FullPath);
-        ZSwapSysFs >> Result;
-    }
-
+    if (!std::filesystem::exists(FullPath)) return std::nullopt;
+    unsigned long Result;
+    std::ifstream ZSwapSysFs(FullPath);
+    ZSwapSysFs >> Result;
     return Result;
 }
