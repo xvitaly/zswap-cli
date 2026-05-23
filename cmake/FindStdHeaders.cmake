97780c4ba8943bd15cfc69e82c36bec87c288459
Vitaly <vitaly@easycoding.org>
2026-05-23 11:23:31 +0200

    Added std::optional header check to the FindStdHeaders.cmake module.

--- cmake/FindStdHeaders.cmake
+++ cmake/FindStdHeaders.cmake
@@ -17,8 +17,13 @@ check_include_file_cxx(
     STD_FORMAT_HEADER
 )
 
+check_include_file_cxx(
+    "optional"
+    STD_OPTIONAL_HEADER
+)
+
 find_package_handle_standard_args(
     StdHeaders
-    REQUIRED_VARS STD_FILESYSTEM_HEADER STD_FORMAT_HEADER
+    REQUIRED_VARS STD_FILESYSTEM_HEADER STD_FORMAT_HEADER STD_OPTIONAL_HEADER
     REASON_FAILURE_MESSAGE "C++ standard library has no C++20 support!"
 )
