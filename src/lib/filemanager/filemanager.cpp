9d64242e9531dc49c053644ac296d01d45df7ba6
Vitaly <vitaly@easycoding.org>
2026-09-03 14:49:44 +0200

    Added a new empty FileManager namespace for functions for working with files and directories.

---
+++ src/lib/filemanager/filemanager.cpp
@@ -0,0 +1,15 @@
+/**
+ * SPDX-FileCopyrightText: 2020-2026 EasyCoding Team and contributors
+ *
+ * SPDX-License-Identifier: MIT
+*/
+
+/**
+ * @file filemanager/filemanager.cpp
+ * Contains the FileManager namespace implementation.
+*/
+
+#include <filesystem>
+
+#include "filemanager/filemanager.hpp"
+
---
+++ src/lib/filemanager/filemanager.hpp
@@ -0,0 +1,24 @@
+/**
+ * SPDX-FileCopyrightText: 2020-2026 EasyCoding Team and contributors
+ *
+ * SPDX-License-Identifier: MIT
+*/
+
+#ifndef FILEMANAGER_HPP
+#define FILEMANAGER_HPP
+
+/**
+ * @file filemanager/filemanager.hpp
+ * Contains the FileManager namespace definition.
+*/
+
+#include <filesystem>
+
+/**
+ * Namespace with functions for working with files and directories.
+*/
+namespace FileManager
+{
+}
+
+#endif // FILEMANAGER_HPP
