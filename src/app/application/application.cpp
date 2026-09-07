8d800fff4a7b9cc7b81098fb9d0546f650748ddb
Vitaly <vitaly@easycoding.org>
2026-09-07 10:33:08 +0200

    Switched the ExecuteSystemConfig() private method of the Application class to CheckFileExists() use.

--- src/app/application/application.cpp
+++ src/app/application/application.cpp
@@ -29,6 +29,7 @@
 #include "appconstants/appconstants.hpp"
 #include "application/application.hpp"
 #include "cwrappers/cwrappers.hpp"
+#include "filemanager/filemanager.hpp"
 #include "ksysinfo/ksysinfo.hpp"
 #include "ksysversion/ksysversion.hpp"
 #include "zswapdebug/zswapdebug.hpp"
@@ -297,7 +298,7 @@ int Application::ExecuteSystemConfig() const
     {
         const std::string ConfigFile = std::format("{0}/{1}/{2}", Prefix, AppConstants::ProductName(), AppConstants::ConfigFileName());
         if (IsVerbose) std::cout << std::format("Checking the \"{0}\" path as a potential config file.", ConfigFile) << std::endl;
-        if (std::filesystem::exists(ConfigFile))
+        if (FileManager::CheckFileExists(ConfigFile))
             return ExecuteConfig(ConfigFile);
     }
 
